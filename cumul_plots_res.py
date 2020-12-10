ee7004aa004aeae65e0ac721568cd178fd580663
J'Neil Blough-Swingen <jneil.cottle@gmail.com>
2020-12-09 17:32:42 -0800

    test resolution by mach number

--- cumul_plots_res.py
+++ cumul_plots_res.py
@@ -10,44 +10,56 @@ pur = '#b63fec'
 black = 'black'
 pink = 'magenta'
 
-size = 7880*2
+clouds = 5
+size8 = 31436*clouds
+size4 = 7859*clouds
+size2 = 1965*clouds
+size1 = 491*clouds
 
-colors = {'H I': black, 'Mg II':blu, 'C III':org, 'C IV':red, 'N V':gre, 'O VI':pur, 'Ne VIII':pink}
+colors = {'H I': black, 'Mg II':blu, 'C III':gre, 'C IV':org, 'N V':red, 'O VI':pur, 'Ne VIII':pink}
 style = {800:'solid', 400:'dashed', 200:'dotted', 100:'dashdot'}
-res = [800, 400, 200, 100]
-M1 = pd.read_csv('testRes_m1.csv')
-M6 = pd.read_csv('testRes_m6.csv')
-M3 = pd.read_csv('testRes_m3.csv')
+machs = ['m1', 'm3', 'm6']
+#M1 = pd.read_csv('testRes_m1.csv')
+#M6 = pd.read_csv('testRes_m6.csv')
+#M3 = pd.read_csv('testRes_m3.csv')
 
  #M1
-fig, ax = plt.subplots(4,1,figsize = (8, 8))
+fig, ax = plt.subplots(3,1,figsize = (9, 10))
 
 for ion in ['Mg II','C IV','O VI']:
-    for i in range(len(res)):
-        coldens_M1 = M1[ion+'_'+str(res[i])]
-        coldens_M3 = M3[ion+'_'+str(res[i])]
-        coldens_M6 = M6[ion+'_'+str(res[i])]
-        coldens_sort_M1 = np.sort(np.log10(coldens_M1))
-        coldens_sort_M3 = np.sort(np.log10(coldens_M3))
-        coldens_sort_M6 = np.sort(np.log10(coldens_M6))
+    for i in range(len(machs)):
+        M = pd.read_csv('testRes_'+machs[i]+'.csv')
+        coldens_800 = M[ion+'_800']
+        coldens_400 = M[ion+'_400']
+        coldens_200 = M[ion+'_200']
+        coldens_100 = M[ion+'_100']
+        coldens_sort_800 = np.sort(np.log10(coldens_800))
+        coldens_sort_400 = np.sort(np.log10(coldens_400))
+        coldens_sort_200 = np.sort(np.log10(coldens_200))
+        coldens_sort_100 = np.sort(np.log10(coldens_100))
+        print(coldens_sort_400)
 
-        #y2 = np.arange(len(coldens_sort_M1[-size:])+1, 1, -1)/(0.5*len(coldens_sort_M1[-size:]))
-        y2 = np.arange(len(coldens_sort_M1)+1, 1, -1)/(0.5*len(coldens_sort_M1))
+        #y = np.arange(len(coldens_sort_M1[-size:])+1, 1, -1)/(0.5*len(coldens_sort_M1[-size:]))
+        y8 = np.arange(len(coldens_sort_800[-size8:])+1, 1, -1)/(len(coldens_sort_800[-size8:])/clouds)
+        y4 = np.arange(len(coldens_sort_400[-size4:])+1, 1, -1)/(len(coldens_sort_400[-size4:])/clouds)
+        y2 = np.arange(len(coldens_sort_200[-size2:])+1, 1, -1)/(len(coldens_sort_200[-size2:])/clouds)
+        y1 = np.arange(len(coldens_sort_100[-size1:])+1, 1, -1)/(len(coldens_sort_100[-size1:])/clouds)
 
         #ax[i].plot(coldens_sort_M1[-size:], y2, label=ion+'_chem', color = colors[ion])
         #ax[i].plot(coldens_sort_M3[-size:], y2, label=ion+'_tri', linestyle = 'dashed', color = colors[ion])
         #ax[i].plot(coldens_sort_M6[-size:], y2, label=ion+'_tri', linestyle = 'dashed', color = colors[ion])
 
-        ax[i].plot(coldens_sort_M1, y2, label=ion+'_M1', color = colors[ion])
-        ax[i].plot(coldens_sort_M3, y2, label=ion+'_M3', color = colors[ion])
-        ax[i].plot(coldens_sort_M6, y2, label=ion+'_M6', color = colors[ion])
-        ax[i].set_title('Res {} x {}'.format(res[i], res[i]))
-        ax[i].set_ylabel("Area Fraction", fontsize = 13)
-        ax[i].set_xlim(12, 18)
+        ax[i].plot(coldens_sort_800[-size8:], y8, label=ion+'_800', color = colors[ion])
+        ax[i].plot(coldens_sort_400[-size4:], y4, label=ion+'_400', linestyle = 'dashed', color = colors[ion])
+        ax[i].plot(coldens_sort_200[-size2:], y2, label=ion+'_200', linestyle = 'dotted', color = colors[ion])
+        ax[i].plot(coldens_sort_100[-size1:], y1, label=ion+'_100', linestyle = 'dashdot', color = colors[ion])
+        ax[i].set_title('Mach {}'.format(machs[i][1:]))
+        ax[i].set_ylabel("Cloud Area", fontsize = 13)
+        ax[i].set_ylim(0, clouds)
+        ax[i].set_xlim(10, 17)
 
 ax[0].legend(bbox_to_anchor=(1.05, 1))
-ax[1].set_xlim(12, 18)
 #ax[0].set_ylim(0, 1)
-ax[3].set_xlabel('$\log(N_{ion})$', fontsize=13)
+ax[2].set_xlabel('$\log(N_{ion})$', fontsize=13)
 plt.tight_layout()
 fig.savefig('testRes.png')
