bf96dd264e33676c38051c7f8a60acbcb202d717
J'Neil Blough-Swingen <jneil.cottle@gmail.com>
2020-12-23 15:08:24 -0800

    resolution plot finished

--- cumul_plots_res.py
+++ cumul_plots_res.py
@@ -31,7 +31,7 @@ machs = ['m1', 'm3', 'm6_other']
 #M3 = pd.read_csv('testRes_m3.csv')
 
  #M1
-fig, ax = plt.subplots(3,1,figsize = (9, 10))
+fig, ax = plt.subplots(1,3,figsize = (11, 9))
 
 for ion in ['H I 1215','C IV','O VI']:
     for i in range(len(machs)):
@@ -58,41 +58,40 @@ for ion in ['H I 1215','C IV','O VI']:
         col_sort_100 = np.sort(coldens_100)
         print(ion)
         print('M{}'.format(machs[i][1:2]))
-        print('800: {}'.format(np.average(coldens_sort_800[-(size8):])))
-        print('400: {}'.format(np.average(coldens_sort_400[-(size4):])))
-        print('200: {}'.format(np.average(coldens_sort_200[-(size2):])))
-        print('100: {}'.format(np.average(coldens_sort_100[-(size1):])))
-        #print('800: {}'.format(np.sum(col_sort_800[-size8:]*area8)))
-        #print('400: {}'.format(np.sum(col_sort_400[-size4:]*area4)))
-        #print('200: {}'.format(np.sum(col_sort_200[-size2:]*area2)))
-        #print('100: {}'.format(np.sum(col_sort_100[-size1:]*area1)))
+        #print('800: {}'.format(np.average(coldens_sort_800[-(size8):])))
+        #print('400: {}'.format(np.average(coldens_sort_400[-(size4):])))
+        #print('200: {}'.format(np.average(coldens_sort_200[-(size2):])))
+        #print('100: {}'.format(np.average(coldens_sort_100[-(size1):])))
+        print('800: {}'.format(np.sum(col_sort_800[-size8:]*area8)))
+        print('400: {}'.format(np.sum(col_sort_400[-size4:]*area4)))
+        print('200: {}'.format(np.sum(col_sort_200[-size2:]*area2)))
+        print('100: {}'.format(np.sum(col_sort_100[-size1:]*area1)))
 
         #plot
         large100 = []
         for m in np.arange(0, size1):
             for j in range(64):
-                large100.append(coldens_sort_100[-size1:][m])
-        #coldens_sort_800[-size8:][64*np.arange(0, 1*491)]
-        restest = coldens_sort_800[-size8:][-125696:] - large100[-125696:]
-        ax[i].plot(coldens_sort_800[-size8:][-125696:], np.log10(large100[-125696:]*(10**restest)), label=ion+'(800-100)', color = colors[ion])
-        #ax[i].hist(coldens_sort_800[-size8:], bins =70, label = ion+'_800')
-        #ax[i].hist(coldens_sort_400[-size4:], bins = 70, label = ion+'_400')
+                large100.append(col_sort_100[-size1:][m])
 
-        #ax[i].plot(coldens_sort_800[-size8:], y8, label=ion+'_800', color = colors[ion])
+        restest = col_sort_800[-size8:][-125696:] - large100[-125696:]
+        ax[i].plot(np.log10(restest), y8[-125696:],  label=ion+'(800-100)', color = colors[ion])
+
+
+        ax[i].plot(coldens_sort_800[-size8:], y8, label=ion+'_800', color = colors[ion])
         #ax[i].plot(coldens_sort_400[-size4:], y4, label=ion+'_400', linestyle = 'dashed', color = colors[ion])
         #ax[i].plot(coldens_sort_200[-size2:], y2, label=ion+'_200', linestyle = 'dotted', color = colors[ion])
-        #ax[i].plot(coldens_sort_100[-size1:], y1, label=ion+'_100', linestyle = 'dashdot', color = colors[ion])
+        ax[i].plot(coldens_sort_100[-size1:], y1, label=ion+'_100', linestyle = 'dashdot', color = colors[ion])
         ax[i].set_title('Mach {}'.format(machs[i][1:2]))
-        ax[i].set_ylabel("$\log(N_{800} - N_{100})$", fontsize = 13)
+        ax[i].set_xlabel("$\log10(N_{800} - N_{100})$", fontsize = 13)
         ax[i].set_xlim(8, 17)
         #ax[i].set_yscale('log')
-        ax[i].set_ylim(-0.1, 1.4)
+        #ax[i].set_ylim(-0.1, 0.5)
 
-#ax[0].set_ylim(0, clouds)
-#ax[1].set_ylim(0, 4)
-#ax[2].set_ylim(0, 1.5)
-ax[0].legend(bbox_to_anchor=(1.05, 1))
+ax[0].set_ylim(0, 2)
+ax[1].set_ylim(0, 0.5)
+ax[2].set_ylim(0, 0.25)
+ax[2].legend(bbox_to_anchor=(1.05, 1))
 #ax[0].set_ylim(0, 1)
-ax[2].set_xlabel('$\log(N_{ion})$', fontsize=13)
+ax[0].set_ylabel('Cloud Area', fontsize=13)
 plt.tight_layout()
 fig.savefig('resdiff.png')
