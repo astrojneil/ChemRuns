54674a3dca08552cfcbd23937ee37b56bfb79b1d
J'Neil Blough-Swingen <jneil.cottle@gmail.com>
2020-12-23 00:27:33 -0800

    cumul plots for res test and equal cos theta

--- cumul_plots_angle.py
+++ cumul_plots_angle.py
@@ -25,7 +25,7 @@ area1 = 64*pc2
 
 colors = {'H I 1215': black, 'Mg II':blu, 'C III':gre, 'C IV':blu, 'N V':gre, 'O VI':red, 'Ne VIII':pink}
 style = {800:'solid', 400:'dashed', 200:'dotted', 100:'dashdot'}
-machs = ['m1', 'm3', 'm6'] #'m6_other']
+machs = ['nocond', 'lowcond', 'cond'] #'m6_other']
 #M1 = pd.read_csv('testRes_m1.csv')
 #M6 = pd.read_csv('testRes_m6.csv')
 #M3 = pd.read_csv('testRes_m3.csv')
@@ -36,16 +36,16 @@ machs = ['m1', 'm3', 'm6'] #'m6_other']
 for i in range(len(machs)):
     fig, ax = plt.subplots(3,1,figsize = (9, 10))
     for j, ion in enumerate(['H I 1215','C IV','O VI']):
-        M = pd.read_csv('testAngle_'+machs[i]+'.csv')
-        coldens_90 = M[ion+'_90']
-        coldens_80 = M[ion+'_80']
-        coldens_70 = M[ion+'_70']
-        coldens_60 = M[ion+'_60']
-        coldens_50 = M[ion+'_50']
-        coldens_40 = M[ion+'_40']
-        coldens_30 = M[ion+'_30']
-        coldens_20 = M[ion+'_20']
-        coldens_10 = M[ion+'_10']
+        M = pd.read_csv('testAngle_m6_'+machs[i]+'.csv')
+        coldens_90 = M[ion+'_0.9']
+        coldens_80 = M[ion+'_0.8']
+        coldens_70 = M[ion+'_0.7']
+        coldens_60 = M[ion+'_0.6']
+        coldens_50 = M[ion+'_0.5']
+        coldens_40 = M[ion+'_0.4']
+        coldens_30 = M[ion+'_0.3']
+        coldens_20 = M[ion+'_0.2']
+        coldens_10 = M[ion+'_0.1']
         coldens_0 = M[ion+'_0']
         coldens_sort_90 = np.sort(np.log10(coldens_90))
         coldens_sort_80 = np.sort(np.log10(coldens_80))
@@ -61,16 +61,16 @@ for i in range(len(machs)):
         y4 = np.arange(len(coldens_sort_40[-size4:])+1, 1, -1)/(len(coldens_sort_40[-size4:])/clouds)
 
 
-        ax[j].plot(coldens_sort_90[-size4:], y4, label='theta = 90 (y proj)', alpha = 1, color = colors[ion])
-        ax[j].plot(coldens_sort_80[-size4:], y4, label='theta = 80', alpha = 1, color = colors[ion], linestyle = 'dashed')
-        ax[j].plot(coldens_sort_70[-size4:], y4, label='theta = 70', alpha = 0.8, color = colors[ion])
-        ax[j].plot(coldens_sort_60[-size4:], y4, label='theta = 60', alpha = 0.7, color = colors[ion], linestyle = 'dashed')
-        ax[j].plot(coldens_sort_50[-size4:], y4, label='theta = 50', alpha = 0.5, color = colors[ion])
-        ax[j].plot(coldens_sort_40[-size4:], y4, label='theta = 40', alpha = 0.5, color = colors[ion], linestyle = 'dashed')
-        ax[j].plot(coldens_sort_30[-size4:], y4, label='theta = 30', alpha = 0.3, color = colors[ion])
-        ax[j].plot(coldens_sort_20[-size4:], y4, label='theta = 20', alpha = 0.3, color = colors[ion], linestyle = 'dashed')
-        ax[j].plot(coldens_sort_10[-size4:], y4, label='theta = 10', alpha = 0.1, color = colors[ion])
-        ax[j].plot(coldens_sort_0[-size4:], y4, label='theta = 0 (x proj)', alpha = 0.1, color = colors[ion], linestyle = 'dashed')
+        ax[j].plot(coldens_sort_90[-size4:], y4, label='cos(theta) = 0.9 (~ x proj)', alpha = 1, color = colors[ion])
+        ax[j].plot(coldens_sort_80[-size4:], y4, label='cos(theta) = 0.8', alpha = 1, color = colors[ion], linestyle = 'dashed')
+        ax[j].plot(coldens_sort_70[-size4:], y4, label='cos(theta) = 0.7', alpha = 0.8, color = colors[ion])
+        ax[j].plot(coldens_sort_60[-size4:], y4, label='cos(theta) = 0.6', alpha = 0.7, color = colors[ion], linestyle = 'dashed')
+        ax[j].plot(coldens_sort_50[-size4:], y4, label='cos(theta) = 0.5', alpha = 0.5, color = colors[ion])
+        ax[j].plot(coldens_sort_40[-size4:], y4, label='cos(theta) = 0.4', alpha = 0.5, color = colors[ion], linestyle = 'dashed')
+        ax[j].plot(coldens_sort_30[-size4:], y4, label='cos(theta) = 0.3', alpha = 0.3, color = colors[ion])
+        ax[j].plot(coldens_sort_20[-size4:], y4, label='cos(theta) = 0.2', alpha = 0.3, color = colors[ion], linestyle = 'dashed')
+        ax[j].plot(coldens_sort_10[-size4:], y4, label='cos(theta) = 0.1', alpha = 0.1, color = colors[ion])
+        ax[j].plot(coldens_sort_0[-size4:], y4, label='cos(theta) = 0 (y proj)', alpha = 0.1, color = colors[ion], linestyle = 'dashed')
         #ax[j].set_title('Mach {}'.format(machs[i][1:2]))
         ax[j].set_title(ion)
         ax[j].set_ylabel("Cloud Area", fontsize = 13)
--- cumul_plots_res.py
+++ cumul_plots_res.py
@@ -10,11 +10,11 @@ pur = '#b63fec'
 black = 'black'
 pink = 'magenta'
 
-clouds = 5
-size8 = 31436*clouds
-size4 = 7859*clouds
-size2 = 1965*clouds
-size1 = 491*clouds
+clouds = 4
+size8 = round(31436*clouds)
+size4 = round(7859*clouds)
+size2 = round(1965*clouds)
+size1 = round(491*clouds)
 
 
 pc2 = 9.52e36 #cm^2
@@ -57,26 +57,42 @@ for ion in ['H I 1215','C IV','O VI']:
         col_sort_200 = np.sort(coldens_200)
         col_sort_100 = np.sort(coldens_100)
         print(ion)
-        print(machs[i])
-        print('800: {}'.format(np.sum(col_sort_800[-size8:]*area8)))
-        print('400: {}'.format(np.sum(col_sort_400[-size4:]*area4)))
-        print('200: {}'.format(np.sum(col_sort_200[-size2:]*area2)))
-        print('100: {}'.format(np.sum(col_sort_100[-size1:]*area1)))
+        print('M{}'.format(machs[i][1:2]))
+        print('800: {}'.format(np.average(coldens_sort_800[-(size8):])))
+        print('400: {}'.format(np.average(coldens_sort_400[-(size4):])))
+        print('200: {}'.format(np.average(coldens_sort_200[-(size2):])))
+        print('100: {}'.format(np.average(coldens_sort_100[-(size1):])))
+        #print('800: {}'.format(np.sum(col_sort_800[-size8:]*area8)))
+        #print('400: {}'.format(np.sum(col_sort_400[-size4:]*area4)))
+        #print('200: {}'.format(np.sum(col_sort_200[-size2:]*area2)))
+        #print('100: {}'.format(np.sum(col_sort_100[-size1:]*area1)))
 
         #plot
-        ax[i].plot(coldens_sort_800[-size8:], y8, label=ion+'_800', color = colors[ion])
-        ax[i].plot(coldens_sort_400[-size4:], y4, label=ion+'_400', linestyle = 'dashed', color = colors[ion])
-        ax[i].plot(coldens_sort_200[-size2:], y2, label=ion+'_200', linestyle = 'dotted', color = colors[ion])
-        ax[i].plot(coldens_sort_100[-size1:], y1, label=ion+'_100', linestyle = 'dashdot', color = colors[ion])
-        ax[i].set_title('Mach {}'.format(machs[i][1:2]))
-        ax[i].set_ylabel("Cloud Area", fontsize = 13)
-        ax[i].set_xlim(8, 17)
+        large100 = []
+        for m in np.arange(0, size1):
+            for j in range(64):
+                large100.append(coldens_sort_100[-size1:][m])
+        #coldens_sort_800[-size8:][64*np.arange(0, 1*491)]
+        restest = coldens_sort_800[-size8:][-125696:] - large100[-125696:]
+        ax[i].plot(coldens_sort_800[-size8:][-125696:], np.log10(large100[-125696:]*(10**restest)), label=ion+'(800-100)', color = colors[ion])
+        #ax[i].hist(coldens_sort_800[-size8:], bins =70, label = ion+'_800')
+        #ax[i].hist(coldens_sort_400[-size4:], bins = 70, label = ion+'_400')
 
-ax[0].set_ylim(0, clouds)
-ax[1].set_ylim(0, 4)
-ax[2].set_ylim(0, 1.5)
+        #ax[i].plot(coldens_sort_800[-size8:], y8, label=ion+'_800', color = colors[ion])
+        #ax[i].plot(coldens_sort_400[-size4:], y4, label=ion+'_400', linestyle = 'dashed', color = colors[ion])
+        #ax[i].plot(coldens_sort_200[-size2:], y2, label=ion+'_200', linestyle = 'dotted', color = colors[ion])
+        #ax[i].plot(coldens_sort_100[-size1:], y1, label=ion+'_100', linestyle = 'dashdot', color = colors[ion])
+        ax[i].set_title('Mach {}'.format(machs[i][1:2]))
+        ax[i].set_ylabel("$\log(N_{800} - N_{100})$", fontsize = 13)
+        ax[i].set_xlim(8, 17)
+        #ax[i].set_yscale('log')
+        ax[i].set_ylim(-0.1, 1.4)
+
+#ax[0].set_ylim(0, clouds)
+#ax[1].set_ylim(0, 4)
+#ax[2].set_ylim(0, 1.5)
 ax[0].legend(bbox_to_anchor=(1.05, 1))
 #ax[0].set_ylim(0, 1)
 ax[2].set_xlabel('$\log(N_{ion})$', fontsize=13)
 plt.tight_layout()
-fig.savefig('testRes.png')
+fig.savefig('resdiff.png')
