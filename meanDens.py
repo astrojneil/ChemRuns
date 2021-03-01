0e9b860c88647bf1d0ef0352c14f77a78597b60c
J'Neil Blough-Swingen <jneil.cottle@gmail.com>
2021-03-01 14:46:58 -0800

    mean density calc

---
+++ meanDens.py
@@ -0,0 +1,30 @@
+import yt
+import numpy as np
+import os
+
+run1 = {'Name':'M1-v480-T1-chem', 'Mach':1, 'tcc':6.4}
+run2 = {'Name':'M6.2-v3000-T1-chem', 'Mach':6.2, 'tcc':1.0}
+run3 = {'Name':'M3.6-v3000-T3-chem', 'Mach':3.6, 'tcc':1.8}
+
+runList = []
+runList.append(run1)
+runList.append(run2)
+runList.append(run3)
+
+for run in runList:
+    outfile = open(run['Name']+'_meanDens.txt', 'w')
+    outfile.write('Time,meanDens\n')
+    fileList = os.listdir('../'+run['Name']+'/chkfiles')
+    for chk in fileList:
+
+        data = yt.load('CT_hdf5_chk_'+i)
+    	dataRegion = data.all_data()
+    	cloud_region1 = dataRegion.cut_region(['obj["blob"] > 0.'])
+
+    	cloud_region2 = cloud_region1.cut_region(['obj["density"] > 1e-24/3.'])
+    	mean_dens = cloud_region2.quantities.weighted_average_quantity("density", "ones")
+
+    	time = data.current_time.in_units('Myr')/run['tcc']
+
+        outfile.write(str(time)+', \t'+str(mean_dens)+'\n')
+    outfile.close()
